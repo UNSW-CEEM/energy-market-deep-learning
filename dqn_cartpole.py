d3e85717daa3257d809e5fe5dbc3649aa59dbf26
Luke Marshall <lukasmarshall@gmail.com>
2019-02-03 09:41:08 +0900

    turned off visualisation, run dqn cartpole on floydhub

--- dqn_cartpole.py
+++ dqn_cartpole.py
@@ -43,10 +43,10 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=50000, visualize=False, verbose=2)
+dqn.fit(env, nb_steps=500, visualize=False, verbose=2)
 
 # After training is done, we save the final weights.
 dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
-dqn.test(env, nb_episodes=5, visualize=True)
+dqn.test(env, nb_episodes=5, visualize=False)
